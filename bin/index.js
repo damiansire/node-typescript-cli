256e1dc3d10ef14d0046b6a831ce5af973263590
Damian Sire <damiansire@gmail.com>
2024-08-25 16:22:02 -0300

    feat: add base question for create package.json

--- bin/index.js
+++ bin/index.js
@@ -2,28 +2,31 @@
 const yargs = require("yargs");
 const utils = require("./utils.js");
 const readline = require("readline");
+const { exec } = require("child_process");
+const fs = require("fs").promises; // Usar la versión de promesas de fs
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-(async () => {
-  const { default: chalk } = await import("chalk");
-  const { default: boxen } = await import("boxen");
+rl.question("Enter package name: ", async (packageName) => {
+  try {
+    await exec("npm init --yes");
 
-  console.log(
-    "\n" +
-      boxen(chalk.green("\n" + "hello this is a test" + "\n"), {
-        padding: 1,
-        borderColor: "green",
-        dimBorder: true,
-      }) +
-      "\n"
-  );
+    const data = await fs.readFile("package.json", "utf8");
+    const packageJson = JSON.parse(data);
 
-  rl.question("Enter package name: ", (packageName) => {
+    packageJson.name = packageName;
+    packageJson.description = "Your project description here";
+
+    await fs.writeFile("package.json", JSON.stringify(packageJson, null, 2));
+
     utils.createPackage(packageName);
     rl.close();
-  });
-})();
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+});
