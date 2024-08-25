e1436cf6216436392a3ecf29160902af8776900b
Damian Sire <damiansire@gmail.com>
2024-08-25 16:42:06 -0300

    feat: the user can set packageName and packageDescription in sync way

--- bin/index.js
+++ bin/index.js
@@ -3,30 +3,40 @@ const yargs = require("yargs");
 const utils = require("./utils.js");
 const readline = require("readline");
 const { exec } = require("child_process");
-const fs = require("fs").promises; // Usar la versión de promesas de fs
+const fs = require("fs").promises;
 const util = require("util");
-const exec = util.promisify(require("child_process").exec);
 
+const execPromise = util.promisify(exec);
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("Enter package name: ", async (packageName) => {
+const question = (query) => {
+  return new Promise((resolve) => rl.question(query, resolve));
+};
+
+const main = async () => {
   try {
-    await exec("npm init --yes");
+    const packageName = await question("Enter package name: ");
+    const packageDescription = await question("Enter package description: ");
+
+    await execPromise("npm init --yes");
 
     const data = await fs.readFile("package.json", "utf8");
     const packageJson = JSON.parse(data);
 
     packageJson.name = packageName;
-    packageJson.description = "Your project description here";
+    packageJson.description = packageDescription;
 
     await fs.writeFile("package.json", JSON.stringify(packageJson, null, 2));
 
     utils.createPackage(packageName);
-    rl.close();
   } catch (error) {
     console.error(`Error: ${error}`);
+  } finally {
+    rl.close();
   }
-});
+};
+
+main();
